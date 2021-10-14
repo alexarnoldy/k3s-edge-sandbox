a4bbc0bbdf5a637a49178505ee68abe74e87b13f
alexarnoldy <alexarnoldy@gmail.com>
2021-10-14 13:28:38 -0600

    building the script

--- KVM/files/files_for_internet_facing_node/ssh-tunnel-deployer.sh
+++ KVM/files/files_for_internet_facing_node/ssh-tunnel-deployer.sh
@@ -3,11 +3,17 @@
 ## Script to set up an ssh tunnel between an EC2 instance and an application running in a local network
 ## 
 ## Before running this script:
-## 1. Create an EC2 instance with an EIP and a FQDN in Route 53
-## 2. Set the EC2 instance's ssh port to 2244 and set `GatewayPorts clientspecified` in /etc/ssh/sshd_config 
-## 3. Restart sshd.service
-## 4. Place the ssh key on this host (ideally in this directory)
-## 5. Set the variables in the variables.sh file
+## 1. Create an EC2 instance with an EIP and a public FQDN in Route 53
+## 2. Place the ssh key for the EC2 instance on this host (ideally in this directory)
+## 3. Set the variables in the variables.sh file (that is in this directory)
+## 4. SSH to the EC2 instance: 
+## 4.a. Update the /etc/ssh/sshd_config file: 
+## 4.a.i. Set the ssh port to 2244 
+## 4.a.ii Change `GatewayPorts` from `no` to `clientspecified` 
+## 4.b. If the EC2 instance will present priviledged ports (below port 1000, i.e. 80 and 443) on the Internet: 
+## 4.b.i. sudo -i
+## 4.b.ii. Remove the script portion (i.e. no-port-forwarding,no...echo;sleep 10" ) in ~/.ssh/authorized_keys file that prevents direct root logins (this obviously has security implications but is required to present priviledged ports. Use ports above 1000 to avoid this additional risk)
+## 5.c. Restart sshd.service
 ##
 ## With those steps completed, run this script 
 
@@ -29,14 +35,16 @@ ssh ${SSH_USER}@${BACK_END_IP} chmod 400 .ssh/${EC2_SSH_KEY_NAME}
 ## Need to ssh from the back end server to the EC2 instance to remove the script in the root authorized_hosts as well as accept the EC2 key, again ssh'ing as the root user to the EC2 root user
 ### Script continues after exiting the ssh session
 
-ssh ${SSH_USER}@${BACK_END_IP} 'bash -s' << EOF
-echo "#!/bin/bash
-## Need to set ${SERVICE_NAME} ${APP_SERVER_IP} ${EC2_SSH_KEY_NAME} ${FRONT_END_IP} before running
-while :
-do nohup ssh -p 2244 -R 0.0.0.0:42422:${APP_SERVER_IP}:22 -R 0.0.0.0:80:${APP_SERVER_IP}:80 -R 0.0.0.0:443:${APP_SERVER_IP}:443 -N -i /home/${SSH_USER}/.ssh/${EC2_SSH_KEY_NAME} ec2-user@${FRONT_END_IP}
-sleep 10
-done" > /tmp/${SERVICE_NAME}-tunnel.sh
-EOF
+
+
+#ssh ${SSH_USER}@${BACK_END_IP} 'bash -s' << EOF
+#echo "#!/bin/bash
+### Need to set ${SERVICE_NAME} ${APP_SERVER_IP} ${EC2_SSH_KEY_NAME} ${FRONT_END_IP} before running
+#while :
+#do nohup ssh -p 2244 -R 0.0.0.0:42422:${APP_SERVER_IP}:22 -R 0.0.0.0:80:${APP_SERVER_IP}:80 -R 0.0.0.0:443:${APP_SERVER_IP}:443 -N -i /home/${SSH_USER}/.ssh/${EC2_SSH_KEY_NAME} ec2-user@${FRONT_END_IP}
+#sleep 10
+#done" > /tmp/${SERVICE_NAME}-tunnel.sh
+#EOF
 
 ssh ${SSH_USER}@${BACK_END_IP} <<EOF
 sudo mv /tmp/${SERVICE_NAME}-tunnel.service /etc/systemd/system/
@@ -44,7 +52,7 @@ sudo mv /tmp/${SERVICE_NAME}-tunnel.sh /usr/local/bin/
 sudo systemctl enable --now ${SERVICE_NAME}-tunnel.service
 EOF
 
-## To install Rancher server as the application:
+echo " ## To install Rancher server as the application:
 ssh ${SSH_USER}@${APP_SERVER_IP} << EOF
 sudo zypper -n in docker
 sudo systemctl enable --now docker.service
@@ -61,7 +69,12 @@ docker run \
   --name rancher \
   -e CATTLE_BOOTSTRAP_PASSWORD=${RANCHER_BOOTSTRAP_PW} \
   rancher/rancher --acme-domain ${FRONT_URL}
-EOF
+EOF"
+
+
+echo "The following step is only needed if you are presenting privileged ports on the EC2 front-end instance."
+echo "If not, just exit out of the ssh session to complete the script"
+read -s -p "When ready, press Enter to ssh to the back-end ssh tunnel server, then ssh to the EC2 front-end instnce with this command: ssh -p 2244 -i ${EC2_SSH_KEY_LOCATION}/${EC2_SSH_KEY_NAME} root@${FRONT_END_IP}. Finally exit out of both ssh sessions to complete the script"
 
 
 
--- KVM/files/files_for_internet_facing_node/variables.sh
+++ KVM/files/files_for_internet_facing_node/variables.sh
@@ -7,11 +7,17 @@ export SERVICE_NAME=""
 ## The IP address of the back-end ssh tunnel server
 export BACK_END_IP="" 
 
-## The back-end ssh tunnel server needs the ssh key to create the ssh tunnel to the front-end ec2 instance
+## This key will bed used by the back-end ssh tunnel server to create the ssh tunnel to the front-end ec2 instance
 export EC2_SSH_KEY_NAME=""
 
-## The location of the ssh key on this host (ideally in this directory)
+## The location of the EC2 instance's ssh key on this host (ideally in this directory)
 export EC2_SSH_KEY_LOCATION=""
+
+## This key will be used to ssh from this host to the back-end ssh tunnel server
+export BACKEND_SSH_KEY_NAME=""
+
+## The location of the back-end ssh tunnel server's ssh key on this host (ideally in this directory)
+export BACKEND_SSH_KEY_LOCATION=""
 
 ## This is the user on the back-end ssh tunnel server
 export SSH_USER=""
@@ -22,6 +28,6 @@ export FRONT_END_IP=""
 ## The IP address of the server that runs the application that will be fronted with the FRONT_URL
 export APP_SERVER_IP="" 
 
-## This is only applicable to Rancher 2.6 apps, and specifically for the docker container version
+## (Optional) This is only applicable when Rancher 2.6 is the appliation to be provided on the Internet
 export RANCHER_BOOTSTRAP_PW=""
 
